cf2e1dcb7f572f46ad50d0f7ff4b174472986c8b
Priyansh Nihalani <138807092+priyanshnihalani@users.noreply.github.com>
2024-05-16 21:36:15 +0530

    React_Js

--- TODO/src/firebase.js
+++ TODO/src/firebase.js
@@ -5,14 +5,6 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDgKs4Hg-z-LEZFUP8kmFEmllddLTh2DM0",
-  authDomain: "todo-53d53.firebaseapp.com",
-  projectId: "todo-53d53",
-  storageBucket: "todo-53d53.appspot.com",
-  messagingSenderId: "964493881897",
-  appId: "1:964493881897:web:3de68e40d0261d591ae238"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
